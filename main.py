2ba7b41fb430c149ea7ec977cc289ff12bccbb8c
Paulo Oliveira <paulo.oliveirasantosfilho@gmail.com>
2025-03-24 23:21:18 -0300

    banco de dados

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from routes import *
+from flask_sqlalchemy import SQLAlchemy
+
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///dados.db"
+db = SQLAlchemy()
+db.init_app(app)
 
-from routes import *
+class pastas_mapeamento(db.Model):
+    __tablename__ = 'folderMapping'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(40), nullable=False, unique=True)
 
 if __name__ == "__main__":
-    app.run()
+    with app.app_context():
+        user = pastas_mapeamento(nome='teste')
+        db.session.add(user)
+        db.session.commit()
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
